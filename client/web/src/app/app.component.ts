5572244468a68d3f546c5a9bcc65218fa1c17bf1
Aneta Ugrinoska <aneta.ugrin@gmail.com>
2024-10-23 21:52:00 +0200

    update app.component

--- client/web/src/app/app.component.ts
+++ client/web/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {AppNavbarComponent} from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    AppNavbarComponent
   ],
-  template: ``,
+  template: `<app-navbar></app-navbar>`,
 })
 export class AppComponent {
 }
