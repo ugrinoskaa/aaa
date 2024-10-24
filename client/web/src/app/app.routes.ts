11b6d1a8d564ee7dd1c5c8575c7d3a68c7a03a75
Aneta Ugrinoska <aneta.ugrin@gmail.com>
2024-10-24 19:21:00 +0200

    home route

--- client/web/src/app/app.routes.ts
+++ client/web/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import {Routes} from '@angular/router';
+import {HomeComponent} from "../../../../../../Desktop/AAA/client/web/src/app/components/home/home.component";
 
 export const routes: Routes = [
-
+  {
+    path: '**',
+    component: HomeComponent
+  }
 ];
